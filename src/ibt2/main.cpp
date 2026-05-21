7c190f9849ffdb904983a6f66c800714ce4aba4d
t-ros <diogo.mm.monteiro@gmail.com>
2026-05-21 15:59:20 +0100

    feat: ibt2 src

--- src/ibt2/main.cpp
+++ src/ibt2/main.cpp
@@ -1,7 +1,46 @@
 #include <Arduino.h>
 
+const int RPWM_PIN = 25; 
+const int LPWM_PIN = 26;
+
+// Na API antiga, precisamos de definir canais (0 a 15)
+const int RPWM_CHANNEL = 0;
+const int LPWM_CHANNEL = 1;
+
+const int freq = 25000;    
+const int resolution = 8;  
+const int dutyCycle30 = 76; // 30% de 255
+
 void setup() {
+  Serial.begin(115200);
+
+
+  // 1. Configurar o canal com a frequência e resolução
+  ledcSetup(RPWM_CHANNEL, freq, resolution);
+  ledcSetup(LPWM_CHANNEL, freq, resolution);
+
+  // 2. Associar o pino ao canal configurado
+  ledcAttachPin(RPWM_PIN, RPWM_CHANNEL);
+  ledcAttachPin(LPWM_PIN, LPWM_CHANNEL);
+
+  Serial.println("Teste a 30% iniciado.");
 }
 
 void loop() {
+  
+  Serial.println("RPWM ativo a 30% (GPIO 25)");
+  // Na API antiga, escreves para o CANAL, não para o pino
+  ledcWrite(LPWM_CHANNEL, 0);
+  ledcWrite(RPWM_CHANNEL, dutyCycle30);
+  
+  // Atraso de 5 segundos obrigatório para não fritar o driver e ver os prints
+  delay(5000);
+
+  // Ativa LPWM a 30% e desliga RPWM
+  Serial.println("LPWM ativo a 30% (GPIO 26)");
+  ledcWrite(RPWM_CHANNEL, 0);
+  ledcWrite(LPWM_CHANNEL, dutyCycle30);
+
+  // Atraso de 5 segundos
+  delay(5000);
 }
--- src/master/README.md
+++ src/master/README.md
@@ -3,4 +3,3 @@
 `web_server.cpp`: Criação de uma rede para a esp.  
 `ina219.cpp`: Controlo das medições no master através do módulo ina219.  
 `comms.cpp`: Ponte de comunicação entre o master e as minis através de ESP-NOW.  
-
