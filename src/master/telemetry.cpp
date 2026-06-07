3249a66ba563239b79315b6c218554d68ad9ce00
t-ros <diogo.mm.monteiro@gmail.com>
2026-06-08 00:28:55 +0100

    refactor: standardize telemetry separators and footer
    Replace '->' markers with '||' in telemetry printouts for Master, Mini 1, and Mini 2 to standardize formatting and improve readability. Also add a closing dashed separator line after the Mini 2 output. No changes to the telemetry values or logic, only output formatting.

--- src/master/telemetry.cpp
+++ src/master/telemetry.cpp
@@ -14,23 +14,24 @@ void imprimirTelemetria() {
     Serial.println("------------------------------------");
     // Mostra as leituras do Master
     DadosEnergia dados_master = lerDadosINA219();
-    Serial.print("MASTER -> ");
+    Serial.print("MASTER || ");
     Serial.print(dados_master.tensao_V); Serial.print(" V | ");
     Serial.print(dados_master.corrente_mA); Serial.print(" mA | ");
     Serial.print(dados_master.potencia_mW); Serial.println(" mW");
 
     // Mostra as leituras do Mini 1
     struct_message m1 = getDadosMini1();
-    Serial.print("MINI 1 -> "); 
+    Serial.print("MINI 1 || "); 
     Serial.print(m1.tensao_V); Serial.print(" V | ");
     Serial.print(m1.corrente_mA); Serial.print(" mA | ");
     Serial.print(m1.potencia_mW); Serial.println(" mW");
 
     // Mostra as leituras do Mini 2
     struct_message m2 = getDadosMini2();
-    Serial.print("MINI 2 -> ");
+    Serial.print("MINI 2 || ");
     Serial.print(m2.tensao_V); Serial.print(" V | ");
     Serial.print(m2.corrente_mA); Serial.print(" mA | ");  
     Serial.print(m2.potencia_mW); Serial.println(" mW");
+    Serial.println("------------------------------------");
   }
 }
